c49b07c404516ce8572d0b53e3fd2dcc8f1d5483
Alex Puscas <alex.puscas@tum.de>
2024-06-01 13:42:00 +0000

    Switch from FMM to STMATCH
    With this, we skip computing the UBODT file entirely, saving 3-4 minutes per file creation. Additionally, STMATCH matches the detector
    faster, again going from ~3 minutes (FMM) to 30-60 seconds (STMATCH)

--- src/match.py
+++ src/match.py
@@ -1,57 +1,33 @@
-from fmm import FastMapMatch, FastMapMatchConfig, Network, NetworkGraph, UBODTGenAlgorithm, UBODT 
-from fmm import GPSConfig, ResultConfig
+from fmm import FastMapMatch, STMATCH, Network, NetworkGraph, UBODTGenAlgorithm, UBODT 
+from fmm import GPSConfig, ResultConfig, STMATCHConfig, FastMapMatchConfig
 from config import RUNS_PER_RELOAD
 
 import sys
 import logging
 
-### Get cl args
-#argscount = 2
-#if len(sys.argv) != argscount+1:
-#    print(("We need {} args, namely: {}, {}".format(argscount, "network_dir", "points")))
-#    quit()
-#network_dir, fpoints = sys.argv[1:]
-#with open(fpoints, "r") as f:
-#    wkt = f.read()
-#    print(wkt)
-
 network_dir = "../data/network"
-runs = 0
 
 
 def match(fpoints: str):
-    # Declare runs as global so we can modify it
-    global runs
+    """
+    Match the detector points defined in the file given by fpoints to the street network defined in edges.shp
 
+    :param fpoints: Path to the csv file containing the detector locations to be matched to the network
+    """
     ### Read network data
     network = Network(network_dir+"/edges.shp","fid","u","v")
-    print("Nodes {} edges {}".format(network.get_node_count(),network.get_edge_count()))
+    logging.info("Nodes {} edges {}".format(network.get_node_count(),network.get_edge_count()))
     graph = NetworkGraph(network)
        
-    ### Precompute an UBODT table  
-    # Can be skipped if you already generated an ubodt file
-    runs %= RUNS_PER_RELOAD
-    if (runs == 0):
-        ubodt_gen = UBODTGenAlgorithm(network,graph)
-        status = ubodt_gen.generate_ubodt(network_dir+"/ubodt.txt", 0.02, binary=False, use_omp=True)
-        print(status)
-        logging.info(f"Precomputed the UBODT table in {__name__}")
-    else:
-        logging.info(f"Did not precompute the UBODT table in {__name__}")
-    runs += 1
-    
-    ### Read UBODT
-    ubodt = UBODT.read_ubodt_csv(network_dir+"/ubodt.txt")
-    
-    ### Create FMM model
-    # TODO: maybe use STMATCH instead? 
-    model = FastMapMatch(network,graph,ubodt)
-    
+    ### Create STMATCH model
+    model = STMATCH(network, graph)
+
     ### Define map matching configurations
     k = 8
     radius = 0.003
     gps_error = 0.0005
-    fmm_config = FastMapMatchConfig(k,radius,gps_error)
+    # TODO: has a vmax default of 30 km/h, read in paper how this value affects the results?
+    stmatch_config = STMATCHConfig(k, radius, gps_error)
 
     ### Define GPS configurations
     gps_config = GPSConfig()
@@ -68,6 +44,6 @@ def match(fpoints: str):
     result_config.output_config.write_opath = True
     
     ### Run map matching for GPS files
-    status = model.match_gps_file(gps_config, result_config, fmm_config)
-    print(status)
+    status = model.match_gps_file(gps_config, result_config, stmatch_config, use_omp=True)
+    logging.info(status)
         
