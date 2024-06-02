e3e0030f5b930f83715fe7e9c94f98a23ba53ae7
Alex Puscas <alex.puscas@tum.de>
2024-06-02 13:44:02 +0000

    Relocate config file

--- src/config.py
+++ src/config.py
@@ -3,6 +3,6 @@ import sys
 
 sys.path.insert(2, os.path.dirname(os.path.abspath(__file__)))
 
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__)).replace("\\", "/")
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))).replace("\\", "/")
 RUNS_PER_RELOAD = 96 * 2 / 96  # number of 15-minute-intervales multiplied with number 
                                # of days -> run the UBODT pre-computation algorithm in match.py provided by FMM
