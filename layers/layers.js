2280c8b96b6b4be5890fc8fb3792369f14988bd1
Alex Puscas <alex.puscas@tum.de>
2024-08-17 18:12:42 +0200

    Update qgis2web related files

--- layers/layers.js
+++ layers/layers.js
@@ -3,7 +3,7 @@ var wms_layers = [];
 
         var lyr_OSM_0 = new ol.layer.Tile({
             'title': 'OSM',
-            'type': 'base',
+            //'type': 'base',
             'opacity': 1.000000,
             
             
@@ -20,25 +20,19 @@ var jsonSource_OSMnxedges_1 = new ol.source.Vector({
 });
 jsonSource_OSMnxedges_1.addFeatures(features_OSMnxedges_1);
 var lyr_OSMnxedges_1 = new ol.layer.Vector({
-                declutter: true,
+                declutter: false,
                 source:jsonSource_OSMnxedges_1, 
                 style: style_OSMnxedges_1,
                 popuplayertitle: "OSMnx edges",
                 interactive: true,
-    title: 'OSMnx edges<br />\
-    <img src="styles/legend/OSMnxedges_1_0.png" /> No Data<br />\
-    <img src="styles/legend/OSMnxedges_1_1.png" /> Very Low Traffic<br />\
-    <img src="styles/legend/OSMnxedges_1_2.png" /> Low Traffic<br />\
-    <img src="styles/legend/OSMnxedges_1_3.png" /> Normal Traffic<br />\
-    <img src="styles/legend/OSMnxedges_1_4.png" /> Busy Traffic<br />\
-    <img src="styles/legend/OSMnxedges_1_5.png" /> Very Busy Traffic<br />'
-        });
+                title: 'OSMnx edges'
+            });
 
 lyr_OSM_0.setVisible(true);lyr_OSMnxedges_1.setVisible(true);
 var layersList = [lyr_OSM_0,lyr_OSMnxedges_1];
-lyr_OSMnxedges_1.set('fieldAliases', {'fid': 'fid', 'u': 'u', 'v': 'v', 'key': 'key', 'osmid': 'osmid', 'name': 'name', 'highway': 'highway', 'maxspeed': 'maxspeed', 'oneway': 'oneway', 'reversed': 'reversed', 'length': 'length', 'from': 'from', 'to': 'to', 'lanes': 'lanes', 'ref': 'ref', 'bridge': 'bridge', 'access': 'access', 'tunnel': 'tunnel', 'width': 'width', 'junction': 'junction', 'est_width': 'est_width', 'OSMnx nodes_osmid': 'OSMnx nodes_osmid', 'OSMnx nodes_y': 'OSMnx nodes_y', 'OSMnx nodes_x': 'OSMnx nodes_x', 'OSMnx nodes_street_count': 'OSMnx nodes_street_count', 'OSMnx nodes_size': 'OSMnx nodes_size', 'OSMnx nodes_ref': 'OSMnx nodes_ref', 'OSMnx nodes_highway': 'OSMnx nodes_highway', 'OSMnx nodes_flow': 'OSMnx nodes_flow', });
-lyr_OSMnxedges_1.set('fieldImages', {'fid': '', 'u': '', 'v': '', 'key': '', 'osmid': '', 'name': '', 'highway': '', 'maxspeed': '', 'oneway': '', 'reversed': '', 'length': '', 'from': '', 'to': '', 'lanes': '', 'ref': '', 'bridge': '', 'access': '', 'tunnel': '', 'width': '', 'junction': '', 'est_width': '', 'OSMnx nodes_osmid': '', 'OSMnx nodes_y': '', 'OSMnx nodes_x': '', 'OSMnx nodes_street_count': '', 'OSMnx nodes_size': '', 'OSMnx nodes_ref': '', 'OSMnx nodes_highway': '', 'OSMnx nodes_flow': '', });
-lyr_OSMnxedges_1.set('fieldLabels', {'fid': 'no label', 'u': 'no label', 'v': 'no label', 'key': 'no label', 'osmid': 'no label', 'name': 'no label', 'highway': 'no label', 'maxspeed': 'no label', 'oneway': 'no label', 'reversed': 'no label', 'length': 'no label', 'from': 'no label', 'to': 'no label', 'lanes': 'no label', 'ref': 'no label', 'bridge': 'no label', 'access': 'no label', 'tunnel': 'no label', 'width': 'no label', 'junction': 'no label', 'est_width': 'no label', 'OSMnx nodes_osmid': 'no label', 'OSMnx nodes_y': 'no label', 'OSMnx nodes_x': 'no label', 'OSMnx nodes_street_count': 'no label', 'OSMnx nodes_size': 'no label', 'OSMnx nodes_ref': 'no label', 'OSMnx nodes_highway': 'no label', 'OSMnx nodes_flow': 'no label', });
+lyr_OSMnxedges_1.set('fieldAliases', {'fid': 'fid', 'u': 'u', 'v': 'v', 'key': 'key', 'osmid': 'osmid', 'name': 'name', 'highway': 'highway', 'maxspeed': 'maxspeed', 'oneway': 'oneway', 'reversed': 'reversed', 'length': 'length', 'prior_flow': 'prior_flow', 'flow': 'flow', 'from': 'from', 'to': 'to', 'lanes': 'lanes', 'ref': 'ref', 'bridge': 'bridge', 'access': 'access', 'tunnel': 'tunnel', 'width': 'width', 'junction': 'junction', 'est_width': 'est_width', });
+lyr_OSMnxedges_1.set('fieldImages', {'fid': '', 'u': '', 'v': '', 'key': '', 'osmid': '', 'name': '', 'highway': '', 'maxspeed': '', 'oneway': '', 'reversed': '', 'length': '', 'prior_flow': '', 'flow': '', 'from': '', 'to': '', 'lanes': '', 'ref': '', 'bridge': '', 'access': '', 'tunnel': '', 'width': '', 'junction': '', 'est_width': '', });
+lyr_OSMnxedges_1.set('fieldLabels', {'fid': 'no label', 'u': 'no label', 'v': 'no label', 'key': 'no label', 'osmid': 'no label', 'name': 'no label', 'highway': 'no label', 'maxspeed': 'no label', 'oneway': 'no label', 'reversed': 'no label', 'length': 'no label', 'prior_flow': 'no label', 'flow': 'no label', 'from': 'no label', 'to': 'no label', 'lanes': 'no label', 'ref': 'no label', 'bridge': 'no label', 'access': 'no label', 'tunnel': 'no label', 'width': 'no label', 'junction': 'no label', 'est_width': 'no label', });
 lyr_OSMnxedges_1.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
--- styles/OSMnxedges_1_style.js
+++ styles/OSMnxedges_1_style.js
@@ -6,7 +6,7 @@ var style_OSMnxedges_1 = function(feature, resolution){
         feature: feature,
         variables: {}
     };
-    var value = feature.get("OSMnx nodes_flow");
+    var value = '';
     var labelText = "";
     size = 0;
     var labelFont = "10px, sans-serif";
@@ -20,49 +20,75 @@ var style_OSMnxedges_1 = function(feature, resolution){
     if ("" !== null) {
         labelText = String("");
     }
-    if (value >= 0.000000 && value <= 1.000000) {
-            style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(175,175,175,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.988}),
+    
+        function rules_OSMnxedges_1(feature, value) {
+            var context = {
+                feature: feature,
+                variables: {}
+            };
+            // Start of if blocks and style check logic
+            if (exp_OSMnxedges_1rule0_eval_expression(context)) {
+                      return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(211,211,211,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 2.508}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
-    })]
-                    } else if (value >= 1.100000 && value <= 100.000000) {
-            style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(0,128,0,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 2.508}),
+    })];
+                    }
+                    else if (exp_OSMnxedges_1rule1_eval_expression(context)) {
+                      return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(255,112,160,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 3.8}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
-    })]
-                    } else if (value >= 100.100000 && value <= 200.000000) {
-            style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(0,165,0,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 2.508}),
+    })];
+                    }
+                    else if (exp_OSMnxedges_1rule2_eval_expression(context)) {
+                      return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(0,128,0,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 3.8}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
-    })]
-                    } else if (value >= 200.100000 && value <= 300.000000) {
-            style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(245,255,9,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 2.508}),
+    })];
+                    }
+                    else if (exp_OSMnxedges_1rule3_eval_expression(context)) {
+                      return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(0,165,0,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 6.6499999999999995}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
-    })]
-                    } else if (value >= 300.100000 && value <= 400.000000) {
-            style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(255,166,52,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 2.508}),
+    })];
+                    }
+                    else if (exp_OSMnxedges_1rule4_eval_expression(context)) {
+                      return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(245,255,9,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 9.5}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
-    })]
-                    } else if (value >= 400.100000 && value <= 1000.000000) {
-            style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(255,39,18,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 2.508}),
+    })];
+                    }
+                    else if (exp_OSMnxedges_1rule5_eval_expression(context)) {
+                      return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(255,166,52,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 12.35}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
-    })]
-                    };
+    })];
+                    }
+                    else if (exp_OSMnxedges_1rule6_eval_expression(context)) {
+                      return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(255,39,18,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 15.2}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    }
+            else {
+                return [];
+            }
+        }
+        var style = rules_OSMnxedges_1(feature, value);
+        ;
 
     return style;
 };
